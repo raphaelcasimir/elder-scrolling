9d577b80b3fc69f2034201b05a467bd2821104bf
Raphaël Casimir <mail@raph.io>
2019-02-15 14:26:20 +0100

    Update enable.sh

--- enable.sh
+++ enable.sh
@@ -2,4 +2,4 @@
 
 source ./util/get_mouseID.sh
 
-xinput set-prop $id "libinput Scroll Method Enabled" 0, 0, 1 
+xinput set-prop $id "libinput Scroll Method Enabled" 0, 0, 1 && xinput set-prop $id "libinput Button Scrolling Button" 3
