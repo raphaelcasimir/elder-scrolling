a20362be319dd5c3b4670346f2734b5f1c5d1d38
Administrator <admin@example.com>
2019-07-29 03:28:45 -0700

    Update change_mouse_model.sh so it is more foolproof.

--- change_mouse_model.sh
+++ change_mouse_model.sh
@@ -1,4 +1,5 @@
-#/bin/bash
+#!/usr/bin/env bash
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -6,6 +7,27 @@ echo "${bold}Available pointing devices:${normal}"
 xinput list --name-only
 echo ''
 
-read -p "${bold}Enter your pointing device distinct name (case insensitive), ex: for 'Logitech M570' you can enter M570 if you have multiple Logitech devices:${normal}`echo ' > '`" name
-gawk -i inplace -v name="${name}" -F \" '/"/{gsub($2,name,$0)};{print};' ./util/get_mouseID.sh
-./util/trackball_scroll.sh
+until [[ ! -z $id ]]; do
+	read -p "Enter mouse name to grep: " uname
+	id=$(xinput list --name-only | grep -i $uname)
+	if [ -z "$id" ]; then echo "	No $uname device found. Try again."; fi
+done
+
+echo "	Selected $id."
+
+# Check in what state the device is (es enabled or not)
+ena=$(xinput --list-props "$id" | grep "libinput Scroll Method Enabled (" | cut -d, -f 3)
+
+if [ $ena -eq 0 ]
+then
+	xinput set-prop "$id" "libinput Scroll Method Enabled" 0, 0, 1
+	xinput set-prop "$id" "libinput Button Scrolling Button" 3
+	echo "	Enabled for $id."
+else
+	xinput set-prop "$id" "libinput Scroll Method Enabled" 0, 0, 0
+	echo "	Disabled for $id."
+fi
+
+# Backup file for easy rollback
+cp ./util/get_mouseID.sh ./util/get_mouseID.sh.bak
+gawk -i inplace -v name="${id}" -F \" '/"/{gsub($2,name,$0)};{print};' ./util/get_mouseID.sh
