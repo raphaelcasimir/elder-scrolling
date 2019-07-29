2a2dcb38a2cdb3d6ef191d21a084c99ac3506bf7
Administrator <admin@example.com>
2019-07-29 03:35:06 -0700

    Updated comments in change_mouse_model.sh

--- change_mouse_model.sh
+++ change_mouse_model.sh
@@ -3,11 +3,13 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+# List available pointing devices to the user
 echo "${bold}Available pointing devices:${normal}"
 xinput list --name-only
 echo ''
 
-until [[ ! -z $id ]]; do
+# Ask the user to choose a device
+while [ -z $id ]; do
 	read -p "Enter mouse name to grep: " uname
 	id=$(xinput list --name-only | grep -i $uname)
 	if [ -z "$id" ]; then echo "	No $uname device found. Try again."; fi
@@ -15,19 +17,14 @@ done
 
 echo "	Selected $id."
 
-# Check in what state the device is (es enabled or not)
-ena=$(xinput --list-props "$id" | grep "libinput Scroll Method Enabled (" | cut -d, -f 3)
+# Enable it
+xinput set-prop "$id" "libinput Scroll Method Enabled" 0, 0, 1
+xinput set-prop "$id" "libinput Button Scrolling Button" 3
+echo "	Enabled for $id."
 
-if [ $ena -eq 0 ]
-then
-	xinput set-prop "$id" "libinput Scroll Method Enabled" 0, 0, 1
-	xinput set-prop "$id" "libinput Button Scrolling Button" 3
-	echo "	Enabled for $id."
-else
-	xinput set-prop "$id" "libinput Scroll Method Enabled" 0, 0, 0
-	echo "	Disabled for $id."
-fi
 
 # Backup file for easy rollback
 cp ./util/get_mouseID.sh ./util/get_mouseID.sh.bak
+
+# Change device name in the file that will be installed by the main script
 gawk -i inplace -v name="${id}" -F \" '/"/{gsub($2,name,$0)};{print};' ./util/get_mouseID.sh
