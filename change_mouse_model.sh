8cf1103a7f4ba5be11f3a824dc415e7e0ca996c1
Raphaël Casimir <mail@raph.io>
2021-01-17 15:19:45 +0100

    Use device ID instead of names

--- change_mouse_model.sh
+++ change_mouse_model.sh
@@ -11,20 +11,21 @@ echo ''
 # Ask the user to choose a device
 while [ -z $id ]; do
 	read -p "Enter mouse name to grep: " uname
-	id=$(xinput list --name-only | grep -i $uname)
+	id=$(xinput list | grep -i $uname | head -n1 | cut -d'=' -f2 | cut -f1)
+	devName=$(xinput list --name-only | grep -i $uname | head -n1)
 	if [ -z "$id" ]; then echo "	No $uname device found. Try again."; fi
 done
 
-echo "	Selected $id."
+echo "	Selected $devName, id=$id."
 
 # Enable it
 xinput set-prop "$id" "libinput Scroll Method Enabled" 0, 0, 1
 xinput set-prop "$id" "libinput Button Scrolling Button" 3
-echo "	Enabled for $id."
+echo "	Enabled for $devName, id=$id."
 
 
 # Backup file for easy rollback
 cp ./util/get_mouseID.sh ./util/get_mouseID.sh.bak
 
 # Change device name in the file that will be installed by the main script
-gawk -i inplace -v name="${id}" -F \" '/"/{gsub($2,name,$0)};{print};' ./util/get_mouseID.sh
+gawk -i inplace -v name="${devName}" -F \" '/"/{gsub($2,name,$0)};{print};' ./util/get_mouseID.sh
--- util/get_mouseID.sh
+++ util/get_mouseID.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-id=pointer:$(xinput list --name-only | grep -i "Logitech MX Ergo" | head -n1)
+id=$(xinput list | grep -i "Logitech MX Ergo" | head -n1 | cut -d'=' -f2 | cut -f1)
 
 printf "$id"
